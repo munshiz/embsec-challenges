5b0f41d2870f76a015f16192d5e4c716f0207494
Zawad Munshi <yakuel123@gmail.com>
2020-07-07 22:02:12 +0000

    my work for 7/7

---
+++ intro_c/read_example/gcc/read_example.d
@@ -0,0 +1,7 @@
+gcc/read_example.o: read_example.c /home/jovyan/lib/uart/uart.h \
+ /usr/include/newlib/inttypes.h /usr/include/newlib/newlib.h \
+ /usr/include/newlib/_newlib_version.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/machine/ieeefp.h /usr/include/newlib/sys/features.h \
+ /usr/include/newlib/sys/_intsup.h /usr/include/newlib/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h
